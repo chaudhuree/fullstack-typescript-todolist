c10e5ee921b6d513d47781c03dbce3e115904c8a
chaudhuree <67362262+chaudhuree@users.noreply.github.com>
2024-09-22 06:10:46 +0600

    mui theme setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC, ReactElement } from 'react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { customTheme } from './theme/customTheme';
+const App: FC = (): ReactElement => {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <h1>Dark Theme</h1>
+    </ThemeProvider>
+  );
+};
+
+export default App;
