906a239ae2799f88f6889c427ed6b89346fbe1df
chaudhuree <67362262+chaudhuree@users.noreply.github.com>
2024-09-27 05:40:56 +0600

    completed the task counter component and also created the dummy component for task

--- src/components/taskarea/TaskArea.tsx
+++ src/components/taskarea/TaskArea.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid2';
+import React, { FC, ReactElement } from 'react';
+import { TaskCounter } from '../taskCounter/TaskCounter';
+import {Status} from '../CreateTaskForm/enums/Status';
+import { format } from 'date-fns';
+import { Task } from '../task/Task';
 
-const TaskArea: React.FC = (): React.ReactElement => {
+ const TaskArea: FC = (): ReactElement => {
   return (
-    <Grid size={{ md: 8 }} px={4}>
-      <h2>Content Area</h2>
+    <Grid item md={8} px={4}>
+      <Box mb={8} px={4}>
+        <h2>
+          Status Of Your Tasks As On{' '}
+          {format(new Date(), 'PPPP')}
+        </h2>
+      </Box>
+      <Grid
+        container
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Box>
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <TaskCounter status={Status.todo} count={10} />
+          <TaskCounter status={Status.inProgress} count={12} />
+          <TaskCounter status={Status.completed} count={9} />
+        </Grid>
+        </Box>
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          xs={10}
+          md={8}
+        >
+         <Task />
+         <Task />
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
